a09bba81ee9221183e231ac483966317967540c9
Joe Moudrik <j.moudrik@gmail.com>
2017-07-04 15:12:16 +0200

    add setkeymap

---
+++ bin/setkeymap.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+setxkbmap -option grp:ctrl_shift_toggle 'us,cz(qwerty)'
+#setxkbmap -option grp:ctrl_shift_toggle 'us,cz(qwerty),ru'
+xmodmap -e 'keycode 66= Escape'
+xmodmap -e 'clear Lock'
+xmodmap -e "keycode 166=Prior"
+xmodmap -e "keycode 167=Next"
+xmodmap -e "keycode 96=BackSpace"
+xmodmap -e "remove Mod1 = Alt_R"
+xmodmap -e "add Mod3 = Alt_R"
+xinput --disable 11
