260ba972903e1b90357172e36f48989da104289f
Joe Moudrik <j.moudrik@gmail.com>
2019-08-12 16:38:52 +0200

    bin/watch_bat

---
+++ bin/watch_battery.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+LIMIT=30
+[ -n "$1" ] && LIMIT=$1
+
+while true ; do
+	BAT=$(acpi -bat 2>&1 | sed -n 's/.*Discharging, \([0-9]*\)%,.*/\1/p')
+
+	[ -n "$BAT" ] && {
+		[ "$BAT" -le $LIMIT ] && trumpeta
+	}
+	sleep 1
+done
+
