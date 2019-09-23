2568a1c8a1d360e06fd7349094d9621e3e7db433
Joe Moudrik <j.moudrik@gmail.com>
2019-09-23 11:45:45 +0200

    watch_bat: improve

--- bin/watch_battery.sh
+++ bin/watch_battery.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
+FILE=~/.BATTERY_LOG
 
 LIMIT=30
 [ -n "$1" ] && LIMIT=$1
+
+BAT_PLAYED=
 
 while true ; do
 	BAT=$(acpi -bat 2>&1 | sed -n 's/.*Discharging, \([0-9]*\)%,.*/\1/p')
 
 	[ -n "$BAT" ] && {
-		date
+	 	date --rfc-3339=sec
 		echo $BAT
-		[ "$BAT" -le $LIMIT ] && trumpeta
+	} | tee -a "$FILE"
+
+	[ -n "$BAT" -a "$BAT" -le $LIMIT ] && {
+		[ -z "$BAT_PLAYED" ] && trumpeta
+		BAT_PLAYED="JO"
 	}
 	sleep 1
 done
