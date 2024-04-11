ff1644a3458a33d7ca1fc34a086327bdaa0dbbb9
Josef Moudrik <j.moudrik@gmail.com>
2024-04-11 11:59:33 +0200

    update screenlayouts

--- screenlayout/atb_both_na_vysku_default.sh
+++ screenlayout/atb_both_na_vysku_default.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 xrandr --output eDP-1 --primary --mode 1920x1080 --pos 1440x483 --rotate normal --output DP-1 --mode 2560x1440 --pos 0x0 --rotate left --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output DP-3 --off
 feh --bg-fill ~/obrazky/carina_nebula_jwst_2022.jpg --bg-fill ~/obrazky/inspirace/026-mirror-mirror.jpg &
+pepovo_rozlozeni.sh
